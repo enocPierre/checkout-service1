db6dc4bc40d7b1e1e18306aeaf0ca2e137b017d4
pierreEnoc <pierre_enoc@yahoo.fr>
2022-04-27 20:03:46 -0300

    config project with dependencies

---
+++ mscompra/src/test/java/com/mscompra/mscompra/MscompraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mscompra.mscompra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscompraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
