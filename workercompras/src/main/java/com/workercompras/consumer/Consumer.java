c6b7f4cca141ffed1162ae48d21b157017a2a690
enocPierre <enoc.pierre@mercadolivre.com>
2022-05-06 22:29:05 -0300

    add email notification

--- workercompras/src/main/java/com/workercompras/consumer/Consumer.java
+++ workercompras/src/main/java/com/workercompras/consumer/Consumer.java
@@ -3,7 +3,9 @@ package com.workercompras.consumer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.workercompras.model.Pedido;
+import com.workercompras.service.EmailService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -13,11 +15,13 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+@Slf4j
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 @Component
 public class Consumer {
 
     private final ObjectMapper mapper;
+    private final EmailService emailService;
 
     @RabbitListener(queues = {"${queue.name}"})
     public void consumer(@Payload @NotNull Message message) throws IOException {
